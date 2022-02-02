1798c897ea4d70dfe048e28458d24bba408522bc
Thomas Adjourner <adjourner@KmacBeast.local>
2022-02-01 17:07:49 -0700

    Adding diffusion laziness method and documentation

--- diffusion_curvature/_nbdev.py
+++ diffusion_curvature/_nbdev.py
@@ -2,9 +2,15 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"DiffusionMatrix": "00_core.ipynb",
+         "plot_3d": "00_core.ipynb",
+         "rejection_sample": "01_datasets.ipynb",
+         "torus": "01_datasets.ipynb",
+         "curvature": "02_laziness.ipynb"}
 
-modules = []
+modules = ["core.py",
+           "datasets.py",
+           "laziness.py"]
 
 doc_url = "https://x.riddle.press/diffusion_curvature/"
 
